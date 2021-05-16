3895815a8791eebc34a5bc2af74ac970dcea3583
Jvorwerk75 <17047242@student.hhs.nl>
2021-05-16 23:16:21 +0200

    Made method in ApplicantInfo that choses if an applicant is suited for becoming a OGD employee.

--- src/sample/ApplicantInfo.java
+++ src/sample/ApplicantInfo.java
@@ -5,24 +5,76 @@ public class ApplicantInfo {
     private String officeLocation;
     private Boolean oldOGDWorker;
     private Boolean acceptedOffer = false;
+    private Boolean goodMotivation;
+    private Boolean completedStudy;
     private Boolean fulltime;
 
-    public ApplicantInfo(Integer yearsExperience, String officeLocation, Boolean oldOGDWorker, Boolean fulltime){
+    public ApplicantInfo(Integer yearsExperience, String officeLocation, Boolean oldOGDWorker, Boolean fulltime, Boolean goodMotivation, Boolean completedStudy){
         this.yearsExperience = yearsExperience;
         this.officeLocation = officeLocation;
         this.oldOGDWorker = oldOGDWorker;
         this.fulltime = fulltime;
+        this.goodMotivation = goodMotivation;
+        this.completedStudy = completedStudy;
     }
     public void setYearsExperience(Integer yearsExperience){this.yearsExperience = yearsExperience;}
     public void setOfficeLocation(String officeLocation){this.officeLocation = officeLocation;}
     public void setOldOGDWorker(Boolean oldOGDWorker){this.oldOGDWorker = oldOGDWorker;}
     public void setAcceptedOffer(Boolean acceptedOffer){this.acceptedOffer = acceptedOffer;}
     public void setFulltime(Boolean fulltime){this.fulltime = fulltime;}
+    public void setGoodMotivation(Boolean goodMotivation){this.goodMotivation = goodMotivation;}
+    public void setCompletedStudy(Boolean completedStudy){this.completedStudy = completedStudy;}
     public Integer getYearsExperience(){return yearsExperience;}
     public String getOfficeLocation(){return officeLocation;}
     public Boolean getOldOGDWorker(){return oldOGDWorker;}
     public Boolean getAcceptedOffer(){return acceptedOffer;}
     public Boolean getFulltime(){return fulltime;}
+    public Boolean getGoodMotivation(){return goodMotivation;}
+    public Boolean getCompletedStudy(){return completedStudy;}
 
-
+    public Boolean applicantSuitedForJob(){
+        if(yearsExperience < 4 && yearsExperience > 0){
+            return applicantExperienceCase1();
+        }else if( yearsExperience > 3 && yearsExperience < 10){
+            return applicantExperienceCase2();
+        }else{
+            return applicantExperienceCase3();
+        }
+    }
+    public Boolean applicantExperienceCase1(){
+        if(goodMotivation){
+            if(completedStudy){
+                return true;
+            }else{
+                if(oldOGDWorker){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        }else{
+            if(completedStudy){
+                if(oldOGDWorker){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
+    }
+    public Boolean applicantExperienceCase2(){
+        if(completedStudy || oldOGDWorker){
+            return true;
+        }
+            return false;
+    }
+    public Boolean applicantExperienceCase3(){
+        if(completedStudy || oldOGDWorker || goodMotivation){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- test/SalaryTest.java
+++ test/SalaryTest.java
@@ -8,56 +8,56 @@ class SalaryTest {
     //Test of de equivalentieklasen en randwaarden kloppen bij het bepalen van het salaris
     @Test
     void salaryClass1Test1(){
-        ApplicantInfo applicantInfo = new ApplicantInfo(0, null,null,true);
+        ApplicantInfo applicantInfo = new ApplicantInfo(0, null,null,true, null, null);
         Person person = new Person("Jasper", "Yvo Manfred", "Vorwerk"
                 , "20-3-1998", "Man", "NL19RABO0123456789", "123456789", applicantInfo);
         assertTrue(person.getSalary().getAmount() == 2275.00);
     }
     @Test
     void salaryClass1Test2(){
-        ApplicantInfo applicantInfo = new ApplicantInfo(2, null,null,true);
+        ApplicantInfo applicantInfo = new ApplicantInfo(2, null,null,true,null, null);
         Person person = new Person("Jasper", "Yvo Manfred", "Vorwerk"
                 , "20-3-1998", "Man", "NL19RABO0123456789", "123456789", applicantInfo);
         assertTrue(person.getSalary().getAmount() == 2275.00);
     }
     @Test
     void salaryClass1Test3(){
-        ApplicantInfo applicantInfo = new ApplicantInfo(3, null,null,true);
+        ApplicantInfo applicantInfo = new ApplicantInfo(3, null,null,true, null, null);
         Person person = new Person("Jasper", "Yvo Manfred", "Vorwerk"
                 , "20-3-1998", "Man", "NL19RABO0123456789", "123456789", applicantInfo);
         assertTrue(person.getSalary().getAmount() == 2275.00);
     }
     @Test
     void salaryClass2Test1(){
-        ApplicantInfo applicantInfo = new ApplicantInfo(4, null,null,true);
+        ApplicantInfo applicantInfo = new ApplicantInfo(4, null,null,true,null ,null );
         Person person = new Person("Jasper", "Yvo Manfred", "Vorwerk"
                 , "20-3-1998", "Man", "NL19RABO0123456789", "123456789", applicantInfo);
         assertTrue(person.getSalary().getAmount() == 2500.00);
     }
     @Test
     void salaryClass2Test2(){
-        ApplicantInfo applicantInfo = new ApplicantInfo(7, null,null,true);
+        ApplicantInfo applicantInfo = new ApplicantInfo(7, null,null,true,null, null);
         Person person = new Person("Jasper", "Yvo Manfred", "Vorwerk"
                 , "20-3-1998", "Man", "NL19RABO0123456789", "123456789", applicantInfo);
         assertTrue(person.getSalary().getAmount() == 2500.00);
     }
     @Test
     void salaryClass2Test3(){
-        ApplicantInfo applicantInfo = new ApplicantInfo(9, null,null,true);
+        ApplicantInfo applicantInfo = new ApplicantInfo(9, null,null,true,null, null);
         Person person = new Person("Jasper", "Yvo Manfred", "Vorwerk"
                 , "20-3-1998", "Man", "NL19RABO0123456789", "123456789", applicantInfo);
         assertTrue(person.getSalary().getAmount() == 2500.00);
     }
     @Test
     void salaryClass3Test1(){
-        ApplicantInfo applicantInfo = new ApplicantInfo(10, null,null,true);
+        ApplicantInfo applicantInfo = new ApplicantInfo(10, null,null,true,null, null);
         Person person = new Person("Jasper", "Yvo Manfred", "Vorwerk"
                 , "20-3-1998", "Man", "NL19RABO0123456789", "123456789", applicantInfo);
         assertTrue(person.getSalary().getAmount() == 3000.00);
     }
     @Test
     void salaryClass3Test2(){
-        ApplicantInfo applicantInfo = new ApplicantInfo(17, null,null,true);
+        ApplicantInfo applicantInfo = new ApplicantInfo(17, null,null,true,null,null);
         Person person = new Person("Jasper", "Yvo Manfred", "Vorwerk"
                 , "20-3-1998", "Man", "NL19RABO0123456789", "123456789", applicantInfo);
         assertTrue(person.getSalary().getAmount() == 3000.00);
