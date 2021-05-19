6d77249cc553ae8d7e056d07c86e81869ecd64af
Jvorwerk75 <17047242@student.hhs.nl>
2021-05-19 20:38:35 +0200

    changed "geslacht" to "gender"

--- src/sample/Person.java
+++ src/sample/Person.java
@@ -5,18 +5,18 @@ public class Person {
     private String lastName;
     private String middleName;
     private String birthdate;
-    private String geslacht;
+    private String gender;
     private String iban;
     private String bsn;
     private Salary salary;
     private ApplicantInfo applicantInfo;
 
-    public Person(String firstName, String middleName, String lastName, String birthdate, String geslacht, String iban, String bsn, ApplicantInfo applicantInfo){
+    public Person(String firstName, String middleName, String lastName, String birthdate, String gender, String iban, String bsn, ApplicantInfo applicantInfo){
         this.firstName = firstName;
         this.middleName = middleName;
         this.lastName = lastName;
         this.birthdate = birthdate;
-        this.geslacht = geslacht;
+        this.gender = gender;
         this.iban = iban;
         this.bsn = bsn;
         this.applicantInfo = applicantInfo;
@@ -27,7 +27,7 @@ public class Person {
     public void setFirstName(String firstName){this.firstName = firstName;}
     public void setMiddleName(String middleName){this.middleName = middleName;}
     public void setBirthdate(String birthdate){this.birthdate = birthdate;}
-    public void setGeslacht(String geslacht){this.geslacht = geslacht;}
+    public void setGender(String gender){this.gender = gender;}
     public void setIban(String iban){this.iban = iban;}
     public void setBSN(String bsn){this.bsn = bsn;}
     public String getFullName(){return this.firstName + " " + this.middleName + " " + this.lastName;}
@@ -35,7 +35,7 @@ public class Person {
     public String getMiddleName(){return this.middleName;}
     public String getLastName(){return this.lastName;}
     public String getBirthdate(){return this.birthdate;}
-    public String getGeslacht(){return this.geslacht;}
+    public String getGender(){return this.gender;}
     public String getIban(){return this.iban;}
     public String getBsn(){return this.bsn;}
     public Salary getSalary(){return this.salary;}
